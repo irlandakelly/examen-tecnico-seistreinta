6adeeb96ce5c31ff7541b966bff4f5d25d8e4301
irlanda <irlanda@medicadelaciudad.com>
2023-06-14 15:50:53 -0700

    step 2 - define the events model

---
+++ app/models/event.model.js
@@ -0,0 +1,43 @@
+
+module.exports = (sequelize, Sequelize) => {
+    const Event = sequelize.define("event", {
+      event_id: {
+        type: Sequelize.UUID,
+        primaryKey: true,
+        defaultValue: Sequelize.UUIDV4
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      description: {
+        type: Sequelize.TEXT,
+        allowNull: true
+      },
+      start_time: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      end_time: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      location: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+        allowNull: false
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+        allowNull: false
+      }
+    });
+  
+    return Event;
+  };
+  
